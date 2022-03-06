8726ccf5ee6f7979a6fc427e75a4aaf6656fa846
fortune <basket85r@gmail.com>
2022-03-06 07:21:17 +0000

    add kadai-introduce.rb

---
+++ kadai-introduce.rb
@@ -0,0 +1,11 @@
+lastname = "福原"
+firstname = "塁"
+
+fullname = lastname + firstname
+
+age = 23
+
+introduce_name = fullname + "です。"
+introduce_age = age.to_s + "歳です。"
+
+puts introduce_name + introduce_age
